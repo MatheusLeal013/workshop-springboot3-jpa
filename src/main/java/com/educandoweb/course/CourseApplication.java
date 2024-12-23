d0ff4bdd32c79d3aec94db9853a2b2cff908bc71
Matheus Leal <matheusleal1301@gmail.com>
2024-12-23 15:09:35 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Annotation para configurar o projeto para que ele seja uma aplicação Spring Boot
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
