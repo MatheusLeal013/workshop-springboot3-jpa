611dd145f94e4a4fc1326af9b52d59ba08163f0f
Matheus Leal <matheusleal1301@gmail.com>
2024-12-26 14:50:13 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // recurso web que é implementando por um controlador rest
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping // Indicar que esse método que responde uma requisição do tipo get do http
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
